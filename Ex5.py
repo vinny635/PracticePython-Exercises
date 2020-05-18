f294125a2d8cd9f1996b1d01ac5a3561d05a788d
vinny635 <47914647+vinny635@users.noreply.github.com>
2020-05-19 01:05:23 +0800

    Update Ex5.py

--- Ex5.py
+++ Ex5.py
@@ -21,23 +21,28 @@ print(c)
 # Write this in one line of Python
 import random
 
-a = [random.randrange(1, 41) for element in range(0, random.randint(1, 20))]
-b = [random.randrange(1, 41) for element in range(0, random.randint(1, 20))]
-c = []
+# OPTIMAL ANSWER
+print([element for element in [random.randrange(1, 41) for element in range(0, random.randint(1, 20))] if element in [random.randrange(1, 41) for element in range(0, random.randint(1, 20))]])
+
+# SIMPLER ANSWER
+# a = [random.randrange(1, 41) for element in range(0, random.randint(1, 20))]      
+# b = [random.randrange(1, 41) for element in range(0, random.randint(1, 20))]
+# c = []
+
+# print(a)
+# print(b)
+# [c.append(element) for element in b if element in a]
+# print(c)
+
 
 # Or simply
 # a = range(0, random.randint(1, 20))
 # for element in a:
 #     element = random.randrange(1, 41)
-# 
+#
 # b = range(0, random.randint(1, 20))
 # for element in b:
 #     element = random.randrange(1, 41)
 
 # print(list(a))
 # print(list(b))
-
-print(a)
-print(b)
-
-[c.append(element) for element in b if element in a], print(c)
